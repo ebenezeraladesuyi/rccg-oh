966542cc2bbf8e1884735627598c2544872613c2
ebenezeraladesuyi <ebenezeraladesuyi@gmail.com>
2024-06-20 13:12:48 +0100

    toast

--- src/main.tsx
+++ src/main.tsx
@@ -5,12 +5,15 @@ import './index.css'
 import IsLoading from './pages/isLoading/IsLoading.tsx'
 import { RouterProvider } from 'react-router-dom'
 import { element } from './router/Router.tsx'
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     {/* <App /> */}
       <Suspense fallback={<IsLoading />}>
         <RouterProvider router={element} />
+        <ToastContainer />
       </Suspense>
   </React.StrictMode>,
 )
